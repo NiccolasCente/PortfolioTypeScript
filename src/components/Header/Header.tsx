6e7979bf34ef8757abc48deda5e7ee602a6d1e11
NiccolasCente <niccolascente@hotmail.com>
2024-09-22 22:52:37 -0300

    main almost finished

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,41 @@ import React from "react";
 import ThemeToggle from "../Hooks/ThemeToogle"; 
 import LanguageToggle from "../Hooks/LanguageToogle"; 
 import AnimatedSection from "../Hooks/AnimatedSection";
+import { useLanguage } from "../Hooks/LanguageContext"; 
 
 import "../../assets/GlobalStyle.css";
 import "../../assets/Header.css";
 
 const Header: React.FC = () => {
+  const { language } = useLanguage(); 
+
+  const translations = {
+    'pt-BR': {
+      title: "Niccolas.dev",
+      navItems: {
+        home: "Início",
+        about: "Sobre",
+        projects: "Projetos",
+        contacts: "Contatos"
+      }
+    },
+    'en': {
+      title: "Niccolas.dev",
+      navItems: {
+        home: "Home",
+        about: "About",
+        projects: "Projects",
+        contacts: "Contacts"
+      }
+    }
+  };
+
   return (
     <header>
       <div id="header-container">
         <AnimatedSection animation="fade-in">
           <div className="header-title">
-            <h1>Niccolas.dev</h1>
+            <h1>{translations[language].title}</h1> 
           </div>
         </AnimatedSection>
 
@@ -28,10 +52,10 @@ const Header: React.FC = () => {
           <AnimatedSection animation="fade-in">
             <nav>
               <ul>
-                <li><a href="#home">Início</a></li>
-                <li><a href="#sobre">Sobre</a></li>
-                <li><a href="#projetos">Projetos</a></li>
-                <li><a href="#contatos">Contatos</a></li>
+                <li><a href="#home">{translations[language].navItems.home}</a></li> 
+                <li><a href="#sobre">{translations[language].navItems.about}</a></li>
+                <li><a href="#projetos">{translations[language].navItems.projects}</a></li>
+                <li><a href="#contatos">{translations[language].navItems.contacts}</a></li>
               </ul>
             </nav>
           </AnimatedSection>
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
+import AnimatedSection from "../Hooks/AnimatedSection";
+import { useLanguage } from '../Hooks/LanguageContext'; 
+
+
 import '../../assets/Main.css'; 
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import { faEarthAmericas } from '@fortawesome/free-solid-svg-icons';
-
-import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
-import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedinIn, faGithubAlt, faWhatsapp, } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
-
 
 import ImageMain from "../../assets/MainIcons/ImageMain.jpeg";
 import HTML5Icon from "../../assets/MainIcons/HTML5Icon.png";
@@ -20,9 +19,75 @@ import BootstrapIcon from "../../assets/MainIcons/BootstrapIcon.png";
 import FigmaIcon from "../../assets/MainIcons/FigmaIcon.webp";
 
 const Main: React.FC = () => {
+  const { language } = useLanguage(); 
+
+  const translations = {
+    'pt-BR': {
+      title: "Desenvolvedor Front-End",
+      greeting: "Olá, sou o",
+      name: "Niccolas Cente.",
+      description: "Um Desenvolvedor Front-End apaixonado por Design que reside em São Paulo, Brasil.",
+      techStack: "Tech Stack |"
+    },
+    'en': {
+      title: "Front-End Developer",
+      greeting: "Hello, I'm",
+      name: "Niccolas Cente.",
+      description: "A Front-End Developer passionate about Design living in São Paulo, Brazil.",
+      techStack: "Tech Stack |"
+    }
+  };
+
   return (
     <main>
-      
+      <div className="content-container">
+        <div id="text-container">
+          <h1>
+            {translations[language].title}<br /> React <span>👋</span>
+          </h1>
+          <p id="main-p-text">
+            {translations[language].greeting} <strong>{translations[language].name}</strong>. {translations[language].description}
+          </p>
+          
+          <div id="icon-container">
+            <a href="https://www.linkedin.com/in/niccolas-cente-ba989b23b/" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedinIn} size="2x" />
+            </a>
+            <a href="https://github.com/NiccolasCente" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithubAlt} size="2x" />
+            </a>
+            <a href="https://wa.me/11934747011" target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            </a>
+            <a href="mailto:niccolas.dev@hotmail.com">
+              <FontAwesomeIcon icon={faEnvelope} size="2x" />
+            </a>
+          </div>
+          
+          <div id="skills-container">
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <p style={{ marginRight: '1em' }}>{translations[language].techStack}</p>
+              <div className="skill-pair">
+                <img src={HTML5Icon} alt="HTML5Icon" />
+                <img src={CSS3Icon} alt="CSS3Icon" />
+              </div>
+              <div className="skill-pair" style={{ marginLeft: '20px' }}>
+                <img src={JSIcon} alt="JSIcon" />
+                <img src={TypeScriptIcon} alt="TypeIcon" />
+                <img src={ReactIcon} alt="ReactIcon" />
+              </div>
+              <div className="skill-pair" style={{ marginLeft: '20px' }}>
+                <img src={FigmaIcon} alt="FigmaIcon" />
+                <img src={BootstrapIcon} alt="BootstrapIcon" />
+              </div>
+            </div>
+          </div>
+        </div>
+  
+        <div id="img-main" style={{ marginLeft: '20px' }}>
+          <img src={ImageMain} alt="Foto principal" />
+        </div>
+      </div>
     </main>
   );
 }
